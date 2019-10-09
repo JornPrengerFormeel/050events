70ab74e1b2e3397828cb757016666b40c32422e1
Your Name <you@example.com>
2019-10-09 18:49:12 +0200

    stripped strings from whitespace and defined details

--- scraper-simplon.py
+++ scraper-simplon.py
@@ -3,7 +3,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-#hoe noem je mensen die vanuit het westen naar het noorden komen?
+#bro
+import textwrap
 import requests
 import csv
 from bs4 import BeautifulSoup
@@ -17,7 +18,36 @@ soup = BeautifulSoup(page.text, 'html.parser')
 lijst_van_divs = soup.select('a.item')
 
 for enkele_div in lijst_van_divs:
-    print('Title: ' + str(enkele_div.select_one('div.title').text).replace('\t',' '))
-    print('Date: ' + str(enkele_div.select_one('div.date').text.replace('\n', ' ')))
-    print('Location: ' + str(enkele_div.select_one('div.details').text))
-    print('\n')
+	#location, title, date
+	print 'Location: Simplon'
+	print 'Title: ' + str(enkele_div.select_one('div.title').text).replace('\t',' ')
+	print 'Date: ' + str(enkele_div.select_one('div.date').text.replace('\n', ' '))
+	
+	#define details general
+	rough_details = str(enkele_div.select_one('div.details').text) + 'eGenre'
+	details = rough_details.replace('\n', ' ')
+	#print details
+	
+	#location
+	sLocation = 'Waar: '
+	eLocation = 'Open:'
+	print 'Sub-location: ' + details[details.find(sLocation)+len(sLocation):details.rfind(eLocation)]
+	
+	#time
+	sTime = 'Open: '
+	eTime = 'VVK:'
+	print 'Time: ' + details[details.find(sTime)+len(sTime):details.rfind(eTime)]
+	
+	#price
+	sPrice = 'DVK: '
+	ePrice = 'Aanvang:'
+	print 'Price: ' + details[details.find(sPrice)+len(sPrice):details.rfind(ePrice)]
+	
+	#genre
+	sGenre = 'Genre: '
+	eGenre = 'eGenre'
+	print 'Genre: ' + details[details.find(sGenre)+len(sGenre):details.rfind(eGenre)]
+	
+	#print('Details: ' + textwrap.dedent(details).replace('    ', '').replace('\n', ''))
+
+	print('\n')
